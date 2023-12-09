d4889df1458a5de37bfc99cf50a4951c0359c499
jiwonLee42 <ljw56b@gmail.com>
2023-12-09 13:33:25 +0900

    add helloController

--- moolapp/src/main/java/mool/moolapp/MoolApplication.java
+++ moolapp/src/main/java/mool/moolapp/MoolApplication.java
@@ -1,7 +1,9 @@
 package mool.moolapp;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 public class MoolApplication {
@@ -10,4 +12,13 @@ public class MoolApplication {
 		SpringApplication.run(MoolApplication.class, args);
 	}
 
+}
+
+@RestController
+class HelloController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello";
+	}
 }
