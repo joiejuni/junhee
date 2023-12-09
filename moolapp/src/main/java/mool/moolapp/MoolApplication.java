edd857ec9bc0f75fb079131f52a5bc1647ce373d
jiwonLee42 <ljw56b@gmail.com>
2023-12-09 12:58:14 +0900

    create springboot project

---
+++ moolapp/src/main/java/mool/moolapp/MoolApplication.java
@@ -0,0 +1,13 @@
+package mool.moolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoolApplication.class, args);
+	}
+
+}
---
+++ moolapp/src/test/java/mool/moolapp/MoolApplicationTests.java
@@ -0,0 +1,13 @@
+package mool.moolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
