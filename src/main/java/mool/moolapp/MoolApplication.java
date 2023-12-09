f6f998ff39dea2b62a31878e5f5e2817b6890f62
jiwonLee42 <ljw56b@gmail.com>
2023-12-09 14:01:20 +0900

    fix project structure

---
+++ src/main/java/mool/moolapp/MoolApplication.java
@@ -0,0 +1,24 @@
+package mool.moolapp;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+public class MoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoolApplication.class, args);
+	}
+
+}
+
+@RestController
+class HelloController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello";
+	}
+}
